5b8fb01bf4ea5c6d94896b022b2f32fbdc8d2b63
stiv bennett <s.j.bennett@herts.ac.uk>
2025-12-20 19:09:40 +0000

    edited the mk file

--- midirouter/midirouter.mk
+++ midirouter/midirouter.mk
@@ -3,11 +3,12 @@
 # midirouter
 #
 ######################################
-MIDIROUTER_VERSION = 0.14
-MIDIROUTER_SITE = $(call github,stivb,OilCanSteve/midirouter,$(MIDIROUTER_VERSION))
+MIDIROUTER_VERSION = e5c5373fa5350c5917d63b35a744015cb2ade66e
+MIDIROUTER_SITE = https://github.com/stivb/OilCanSteve.git
+MIDIROUTER_SITE_METHOD = git
 MIDIROUTER_BUNDLES = midirouter.lv2
 
-MIDIROUTER_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) MOD=1 OPTIMIZATIONS="" PREFIX=/usr -C $(@D)/source
+MIDIROUTER_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) MOD=1 OPTIMIZATIONS="" PREFIX=/usr -C $(@D)/midirouter/source
 
 define MIDIROUTER_BUILD_CMDS
 	$(MIDIROUTER_TARGET_MAKE)
@@ -15,6 +16,7 @@ endef
 
 define MIDIROUTER_INSTALL_TARGET_CMDS
 	$(MIDIROUTER_TARGET_MAKE) install DESTDIR=$(TARGET_DIR)
+	cp -rv $(@D)/midirouter.lv2 $(TARGET_DIR)/usr/lib/lv2/
 endef
 
 $(eval $(generic-package))
